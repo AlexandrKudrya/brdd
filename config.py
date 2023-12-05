2d7bf786608fb48cd6c22718bf42df48adad4f69
AlexandrKudrya <alexandr.kudrya@gmail.com>
2023-12-05 21:11:38 +0700

    Clear config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = '6315895355:AAHCSMe1DaE68Qh-dBpydSveZpP8rshmbZQ'
+BOT_TOKEN = ''
 START_MESSAGE = "Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение"
-GIGA_CHAT_AUT = "NDRmYzgwMjgtMTQwMy00ZmM0LTk1YjgtZDczOGM2ZDNiNzI0OmIwOWEzOGFhLWIxYTgtNDg1Yi1hNDBjLWVmNGE4OWIwZDE1YQ=="
+GIGA_CHAT_AUT = ''
 CHAT_SYSTEM = "Ты бот, который должен придумывать поздравление с днём рождения по предеданному имени"
